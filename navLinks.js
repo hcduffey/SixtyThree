ed68a69115341d22a1b98f639877f7b98698ca92
hcduffey <cliff.duffey@pm.me>
2022-04-28 19:44:00 -0400

    fixed bug in nav bar

--- navLinks.js
+++ navLinks.js
@@ -1,23 +1,10 @@
-const authRoutes = [
-    { href: "/parks", title: "Parks"},
-    { href: "/users", title: "Users"},
-    { href: "/login", title: "Login"},
-    { href: "/register", title: "Register"}
-];
-
 const siteRoutes = [
     { href: "/parks", title: "Parks"},
     { href: "/users", title: "Users"}
 ];
 
 let navLinks = function navLinks(req, res, next) {
-    if(!req.session || !req.session.currentUser) {
-        res.locals.routes = authRoutes;
-    }
-    else {
-        res.locals.routes = siteRoutes;
-    }
-
+    res.locals.routes = siteRoutes;
     next();
 }
 
