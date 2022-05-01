5d3af7c9d9cbf7b54c16422add73b0efd499aff1
kellumdavis <kellumdavis@gmail.com>
2022-05-01 16:57:49 -0400

    Finished login/signup page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,13 @@
 const close = document.getElementById('close')
 const modal = document.getElementById('modal')
-
+const info1 = document.querySelector('.info1')
+const info2 = document.querySelector('.info2')
+const info3 = document.querySelector('.info3')
 const closeModal = () => {
     modal.style.display = 'none'
-    
+    info1.style.animationName = 'slideIn'
+    info2.style.animationName = 'slideIn'
+    info3.style.animationName = 'slideIn'
   }
 
   close.addEventListener('click', closeModal)
